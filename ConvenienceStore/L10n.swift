127359fa908ce3ef0a67bf6ef9c47fda33ffb09f
ykyouhei <kyouhei.lab@gmail.com>
2017-11-12 22:57:10 +0900

    iPhone X対応

--- ConvenienceStore/L10n.swift
+++ ConvenienceStore/L10n.swift
@@ -1,35 +1,31 @@
-// Generated using SwiftGen, by O.Halligon — https://github.com/AliSoftware/SwiftGen
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
 
 import Foundation
 
+// swiftlint:disable superfluous_disable_command
 // swiftlint:disable file_length
-// swiftlint:disable line_length
 
-// swiftlint:disable type_body_length
-// swiftlint:disable nesting
-// swiftlint:disable variable_name
-// swiftlint:disable valid_docs
-
+// swiftlint:disable explicit_type_interface identifier_name line_length nesting type_body_length type_name
 enum L10n {
 
   enum Common {
 
     enum Label {
       /// 閉じる
-      static let close = L10n.tr("common.label.close")
+      static let close = L10n.tr("Localizable", "common.label.close")
       /// いいえ
-      static let no = L10n.tr("common.label.no")
+      static let no = L10n.tr("Localizable", "common.label.no")
       /// はい
-      static let yes = L10n.tr("common.label.yes")
+      static let yes = L10n.tr("Localizable", "common.label.yes")
     }
 
     enum Title {
       /// FamilyMart
-      static let familymart = L10n.tr("common.title.familymart")
+      static let familymart = L10n.tr("Localizable", "common.title.familymart")
       /// ローソン
-      static let lawson = L10n.tr("common.title.lawson")
+      static let lawson = L10n.tr("Localizable", "common.title.lawson")
       /// ｾﾌﾞﾝｲﾚﾌﾞﾝ
-      static let seveneleven = L10n.tr("common.title.seveneleven")
+      static let seveneleven = L10n.tr("Localizable", "common.title.seveneleven")
     }
   }
 
@@ -37,9 +33,9 @@ enum L10n {
 
     enum Label {
       /// 違反報告
-      static let report = L10n.tr("itemDetail.label.report")
+      static let report = L10n.tr("Localizable", "itemDetail.label.report")
       /// このコメントに対して違反報告をしますか？
-      static let reportMessage = L10n.tr("itemDetail.label.reportMessage")
+      static let reportMessage = L10n.tr("Localizable", "itemDetail.label.reportMessage")
     }
   }
 
@@ -47,17 +43,17 @@ enum L10n {
 
     enum Label {
       /// 星をタップして評価してください
-      static let reviewHelp = L10n.tr("review.label.reviewHelp")
+      static let reviewHelp = L10n.tr("Localizable", "review.label.reviewHelp")
       /// 送信
-      static let send = L10n.tr("review.label.send")
+      static let send = L10n.tr("Localizable", "review.label.send")
       /// レビューを書く
-      static let writeReview = L10n.tr("review.label.writeReview")
+      static let writeReview = L10n.tr("Localizable", "review.label.writeReview")
     }
 
     enum Template {
       /// 評価件数：%d
       static func reviewCount(_ p1: Int) -> String {
-        return L10n.tr("review.template.reviewCount", p1)
+        return L10n.tr("Localizable", "review.template.reviewCount", p1)
       }
     }
   }
@@ -67,21 +63,19 @@ enum L10n {
     enum Label {
       /// %d円(税込)
       static func includeTaxPrice(_ p1: Int) -> String {
-        return L10n.tr("template.label.includeTaxPrice", p1)
+        return L10n.tr("Localizable", "template.label.includeTaxPrice", p1)
       }
     }
   }
 }
+// swiftlint:enable explicit_type_interface identifier_name line_length nesting type_body_length type_name
 
 extension L10n {
-  fileprivate static func tr(_ key: String, _ args: CVarArg...) -> String {
-    let format = NSLocalizedString(key, comment: "")
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
 
-// swiftlint:enable type_body_length
-// swiftlint:enable nesting
-// swiftlint:enable variable_name
-// swiftlint:enable valid_docs
+private final class BundleToken {}
 
